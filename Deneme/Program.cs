64a037bee6794cc91f622b3c76a0cab6d7235c68
burakbk7 <burakbk7@gmail.com>
2021-08-15 18:17:20 +0300

    Add project files.

---
+++ Deneme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+
+            isimler.Add("Burak");
+            
+            isimler.Add("ALİ");
+
+            
+
+            
+        }
+
+    }
+}
+
+
+
+
+
+
+
